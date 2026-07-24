7e0386bdf1b478b2fe61d32c47a75f76a5835387
Rhutu Sawant <rhutusawant@gmail.com>
2026-07-24 21:24:05 +0530

    Fix formatting and improve restaurant billing script

--- Restaurent billing sys.py
+++ Restaurent billing sys.py
@@ -1,13 +1,9 @@
 #3. Restaurant Billing System
-#
 # Develop a Python program that accepts:
-#
 # Customer Name
 # Three Food Items
 # Price of each Item
-#
 # Display:
-#
 # Ordered Items
 # Total Amount
 # GST
