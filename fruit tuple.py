41fadbdf92c32f1eac83f95e5a15d163142ea014
rhutusawant <rhutusawant@gmail.com>
2026-08-13 23:13:12 +0530

    tuple

---
+++ fruit tuple.py
@@ -0,0 +1,8 @@
+fruits = ("apple", "banana", "mango", "orange")
+
+fruit = input("Enter a fruit: ")
+
+if fruit in fruits:
+    print("Fruit is available!")
+else:
+    print("Fruit is not available.")
