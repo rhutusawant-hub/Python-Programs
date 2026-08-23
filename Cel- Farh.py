896fd8c103899c8bd3c978b17b0cf66158d41085
Rhutu Sawant <rhutusawant@gmail.com>
2026-08-23 16:40:27 +0530

    PPS- Level 1- Basic Coding

---
+++ Cel- Farh.py
@@ -0,0 +1,5 @@
+   
+# 7. Accept temperature in Celsius and convert it into Fahrenheit
+C = float(input("Enter temperature in celsius : "))
+F = (C * 9/5) + 32
+print("The temperature in Fahrenheit is : ", F)
