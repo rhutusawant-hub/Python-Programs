a41893238c848e70b90f25821908e781b2459c88
Rhutu Sawant <rhutusawant@gmail.com>
2026-08-26 07:56:29 +0530

    Add files via upload

---
+++ Hourly salary.py
@@ -0,0 +1,6 @@
+# 11. Accept hours worked and hourly rate and calculate total salary.
+
+hr_worked = int(input("Enter number of hours worked : "))
+hourly_rate = float(input("What is the hourly rate? : "))
+total = hourly_rate * hr_worked
+print(f"Total salaly will be {total}")
