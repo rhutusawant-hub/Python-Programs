9fde2d436b5ed252aaf9565f41d48a39df139a74
Rhutu Sawant <rhutusawant@gmail.com>
2026-09-01 22:27:20 +0530

    Restaurant Bill

---
+++ restaurent bill.py
@@ -0,0 +1,20 @@
+name = input("Enter your name : ")
+food_price = float(input("Enter food price: "))
+quantity = int(input("Enter quantity: "))
+tax_percent = float(input("Enter tax percentage: "))
+
+total = food_price * quantity
+tax = total * tax_percent / 100
+final = total + tax
+
+print(f"""
+    ----------Restaurent----------
+    Customer Name : {name}
+    Order Number : 45
+    ------------------------------
+    Food price = {total}
+    Quantity = {quantity}
+    Tax percentage = {tax_percent}%
+    Final amount = {final}
+    ------------------------------
+      """)
