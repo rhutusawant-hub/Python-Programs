d685ebea2a7e412bd2364b7885b22e25beecc876
rhutusawant <rhutusawant@gmail.com>
2026-09-13 19:24:59 +0530

    Data Type - Complex

---
+++ PPS/Data Types/Complex Numbers/Operations.py
@@ -0,0 +1,14 @@
+# Create two complex numbers and display their addition, subtraction, multiplication, and division.
+
+num1 = 2 + 3j
+num2 = 1 + 2j
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print(f"Addition: {add}")
+print(f"Subtraction: {sub}")
+print(f"Multiplication: {mul}")
+print(f"Division: {div}")
---
+++ PPS/Data Types/Complex Numbers/co ordinate.py
@@ -0,0 +1,9 @@
+# Create a complex number representing a mathematical coordinate and display its real and imaginary components.
+
+coordinate = 4 + 7j
+
+print(f"""
+Complex Coordinate: {coordinate}
+Real Component: {coordinate.real}
+Imaginary Component: {coordinate.imag}
+""")
